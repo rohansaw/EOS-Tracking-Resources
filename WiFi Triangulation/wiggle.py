6b788da346f14bac16cb505cf24efee9ff2e967f
markbader <markbader64@gmail.com>
2022-02-14 18:29:29 +0100

    Change wifi triangulation algorithm from average to geometric median.

--- WiFi Triangulation/wiggle.py
+++ WiFi Triangulation/wiggle.py
@@ -4,6 +4,37 @@ import json
 import sys
 
 
+import numpy as np
+from scipy.spatial.distance import cdist, euclidean
+
+def geometric_median(X, eps=1e-5):
+    y = np.mean(X, 0)
+
+    while True:
+        D = cdist(X, [y])
+        nonzeros = (D != 0)[:, 0]
+
+        Dinv = 1 / D[nonzeros]
+        Dinvs = np.sum(Dinv)
+        W = Dinv / Dinvs
+        T = np.sum(W * X[nonzeros], 0)
+
+        num_zeros = len(X) - np.sum(nonzeros)
+        if num_zeros == 0:
+            y1 = T
+        elif num_zeros == len(X):
+            return y
+        else:
+            R = (T - y) * Dinvs
+            r = np.linalg.norm(R)
+            rinv = 0 if r == 0 else num_zeros/r
+            y1 = max(0, 1-rinv)*T + min(1, rinv)*y
+
+        if euclidean(y, y1) < eps:
+            return y1
+
+        y = y1
+
 def main():
     '''
         Sends a request for each BSSID given as Parameter. The responses are saved in a jsons
@@ -14,9 +45,9 @@ def main():
         BSSIDs = sys.argv[1:]
     else:
         print("Usage: python3 wiggle.py BSSID1 BSSID2 ...")
+        exit()
 
-    latitude = []
-    longitude = []
+    positions = []
 
     for BSSID in BSSIDs:
         headers = {
@@ -43,18 +74,18 @@ def main():
                 json.dump(data, file, ensure_ascii=False, indent=4)
             lat = data['results'][0]['trilat']
             long = data['results'][0]['trilong']
-            latitude.append(lat)
-            longitude.append(long)
+            positions.append([lat, long])
             print(f'https://www.google.com/maps/place/{lat},{long}')
 
         else:
             print(data)
 
-    latitude = sum(latitude) / len(latitude)
-    longitude = sum(longitude) / len(longitude)
+    median_position = geometric_median(np.array(positions))
 
-    webbrowser.open(f'https://www.google.com/maps/place/{latitude},{longitude}')
+    webbrowser.open(f'https://www.google.com/maps/place/{median_position[0]},{median_position[1]}')
+
+    return median_position
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
